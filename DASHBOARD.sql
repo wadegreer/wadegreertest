1d6b54f98fd093ea29ebcee0d628b0e25301bfaa
wadegreer <wade.tillman.greer@gmail.com>
2016-11-29 13:51:54 -0500

    Add files via upload

---
+++ DASHBOARD.sql
@@ -0,0 +1,12 @@
+call invn_view_work.`Verification_data_id_month`();
+
+call invn_view_work.Cash_Cost_Class ();
+
+call invn_view_work.`Job_data_id_month`();
+
+call invn_view_work.`Bank_data_id_month`();
+
+call invn_view_work.`Job_bank_cash_class`();
+
+call invn_view_work.dashboard_purchase_id_month();
+
